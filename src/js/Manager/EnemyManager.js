42e06e7f8bd7f1d57b98d19298fa1857f4b71262
Eri <izutsu812@gmail.com>
2020-06-05 17:42:27 +0900

    no message

--- src/js/Manager/EnemyManager.js
+++ src/js/Manager/EnemyManager.js
@@ -11,38 +11,43 @@ export default class EnemyManager extends CommonBase {
     constructor (){
         super ();
         this.enemysList = []; // Enemy管理配列
-        this.enemysList.push (new Enemy()); //1体だけ生成
+        this.enemysList.push (new Enemy()); 
         this.enemysList.push (new Enemy2()); //2体目
 
-        // Enemyを自動で5体生産
+        // Enemyを一定間隔で5体生産
         this.production = setInterval(() => {
+            const enemy = new Enemy();
             this.enemysList.push (new Enemy());
+            this.setListener(enemy);
         }, 4000);
-        
         this.stopProduction();
-
         this.enemysList.forEach(enemy => {
+            this.setListener(enemy);
+        });
+    }
 
-            // Enemyが死んだ時のリスナー
-            enemy.addEventListener ('death', (e) => {
-                // this.enemysListから死んだenemy を削除
-                this.enemysList = this.enemysList.filter(ene => ene != enemy);
-            });
-
-            // 弾に当たった時のスコア通知をEnemyから受け取る
-            // さらにMainMnagaerに報告する
-            enemy.addEventListener('addScore', (e) => {
-                this.dispatchEvent(new CustomEvent('addScore', {detail: e.detail}));
-            });
-
+    setListener(enemy) {
+        // Enemyが死んだ時のリスナー
+        enemy.addEventListener ('death', (e) => {
+            // this.enemysListから死んだenemy を削除
+            this.enemysList = this.enemysList.filter(ene => ene != enemy);
         });
 
+        // 弾に当たった時のスコア通知をEnemyから受け取る
+        // さらにMainMnagaerに報告する
+        enemy.addEventListener('addScore', (e) => {
+            this.dispatchEvent(new CustomEvent('addScore', {detail: e.detail}));
+        });
     }
 
     // 20000秒で5体生産
     stopProduction() {
-        clearInterval(this.production, 20000);
+        setTimeout(() => {
+            clearInterval(this.production, 20000);
+        }, 20000);
     }
+
+
 
     /**
      * EnterFrame.jsの中で
